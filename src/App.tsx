ab02443b71deef652d2588a2b97e15ea1536766d
gyojinnK <ksmug1998@gmail.com>
2024-10-10 16:00:26 +0900

    feat: router 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
 function App() {
-  return (
-    <div className="w-screen h-screen">
-      <h1 className="flex justify-center w-full h-full items-center text-3xl font-[exo]">
-        React in Depth
-      </h1>
-    </div>
-  );
+  return <></>;
 }
 
 export default App;
